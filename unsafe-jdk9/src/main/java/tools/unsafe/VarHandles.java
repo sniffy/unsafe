2972fd2f9401ca9d506079cd8e1ec8d56d6fedf8
bedrin <dmitry.bedrin@gmail.com>
2023-01-19 21:05:48 +0100

    -Pjdk8minus

--- unsafe-jdk9/src/main/java/tools/unsafe/VarHandles.java
+++ unsafe-jdk9/src/main/java/tools/unsafe/VarHandles.java
@@ -1,18 +1,17 @@
 package tools.unsafe;
 
-import java.lang.invoke.VarHandle;
-import java.lang.reflect.Field;
+//import java.lang.invoke.VarHandle;
 
 public final class VarHandles {
 
     private VarHandles() {
     }
 
-    private static VarHandle enableFinalAccess(Field field) throws Exception {
+    /*private static VarHandle enableFinalAccess(Field field) throws Exception {
         Reflections.setAccessible(field);
         Reflections.removeFinalModifier(field);
         return Lookups.trustedLookup().unreflectVarHandle(field);
         // TODO: fallback to privateLookupIn (?)
-    }
+    }*/
 
 }
