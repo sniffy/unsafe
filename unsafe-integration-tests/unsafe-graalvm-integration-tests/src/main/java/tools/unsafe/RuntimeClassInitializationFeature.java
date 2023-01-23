8f2666186bd1c38335d7200e6ce9337e2362f8bf
bedrin <dmitry.bedrin@gmail.com>
2023-01-23 20:56:40 +0100

    //RuntimeClassInitialization.initializeAtRunTime(SampleClass.class);

--- unsafe-integration-tests/unsafe-graalvm-integration-tests/src/main/java/tools/unsafe/RuntimeClassInitializationFeature.java
+++ unsafe-integration-tests/unsafe-graalvm-integration-tests/src/main/java/tools/unsafe/RuntimeClassInitializationFeature.java
@@ -2,14 +2,13 @@ package tools.unsafe;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 
 @AutomaticFeature
 public class RuntimeClassInitializationFeature implements Feature {
 
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
-        RuntimeClassInitialization.initializeAtRunTime(SampleClass.class);
+        //RuntimeClassInitialization.initializeAtRunTime(SampleClass.class);
     }
 
 }
