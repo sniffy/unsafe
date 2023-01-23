2d1af2878abf06d28350c7e5def44b2570f953b1
bedrin <dmitry.bedrin@gmail.com>
2023-01-23 21:03:55 +0100

    registerMethodOverrideReachabilityHandler

--- unsafe-integration-tests/unsafe-graalvm-integration-tests/src/main/java/tools/unsafe/RuntimeClassInitializationFeature.java
+++ unsafe-integration-tests/unsafe-graalvm-integration-tests/src/main/java/tools/unsafe/RuntimeClassInitializationFeature.java
@@ -2,6 +2,12 @@ package tools.unsafe;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
+import tools.unsafe.reflection.FieldSupplier;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @AutomaticFeature
 public class RuntimeClassInitializationFeature implements Feature {
@@ -9,6 +15,27 @@ public class RuntimeClassInitializationFeature implements Feature {
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
         //RuntimeClassInitialization.initializeAtRunTime(SampleClass.class);
+        try {
+            access.registerMethodOverrideReachabilityHandler(
+                    (duringAnalysisAccess, executable) -> {
+                        try {
+                            if (executable instanceof Method) {
+                                Method method = (Method) executable;
+                                Field field = (Field) method.invoke(null);
+                                Class<?> declaringClass = field.getDeclaringClass();
+                                RuntimeClassInitialization.initializeAtRunTime(declaringClass);
+                            }
+                        } catch (InvocationTargetException e) {
+                            throw new RuntimeException(e);
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                    },
+                    FieldSupplier.class.getMethod("field")
+            );
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
