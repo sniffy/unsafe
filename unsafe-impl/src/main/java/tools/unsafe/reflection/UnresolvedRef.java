02bbe3b7d1b1e59f8cda7df37ef49286a2e6d738
bedrin <dmitry.bedrin@gmail.com>
2023-01-23 20:34:24 +0100

    ReferenceFieldHandle

---
+++ unsafe-impl/src/main/java/tools/unsafe/reflection/UnresolvedRef.java
@@ -0,0 +1,121 @@
+package tools.unsafe.reflection;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Wraps a pair of non-null object of class T and exception which could have been thrown when obtaining this object.
+ * Nullable method `T getRef()` is missing intentionally since the idea is to enforcer developer to catch the potential checked exception of type {@link UnresolvedRefException}.
+ *
+ * @param <T>
+ */
+public class UnresolvedRef<T> {
+
+    private final Future<T> future;
+    private final AtomicReference<RefOrException<T>> refOrExceptionAtomicReference = new AtomicReference<RefOrException<T>>();
+
+    protected UnresolvedRef(@Nonnull Callable<T> refSupplier) {
+        future = new CallableFuture<T>(refSupplier);
+    }
+
+    public @Nonnull T resolve() throws Throwable /* or what? */ {
+        RefOrException<T> refOrException = getRefOrException();
+        if (isResolved()) {
+            //noinspection ConstantConditions
+            return refOrException.ref;
+        } else {
+            assert refOrException.throwable != null;
+            throw new RuntimeException(refOrException.throwable); // TODO: or throw what?
+        }
+    }
+
+    private RefOrException<T> getRefOrException() {
+        RefOrException<T> refOrException = refOrExceptionAtomicReference.get();
+        if (null == refOrException) {
+            synchronized (refOrExceptionAtomicReference) {
+                refOrException = refOrExceptionAtomicReference.get();
+                if (null == refOrException) {
+                    T ref = null;
+                    Throwable throwable = null;
+                    try {
+                        ref = future.get();
+                    } catch (ExecutionException e) {
+                        throwable = e.getCause();
+                    } catch (InterruptedException e) {
+                        throwable = e;
+                    }
+                    refOrException = new RefOrException<T>(ref, throwable);
+                    refOrExceptionAtomicReference.set(refOrException);
+                }
+            }
+        }
+        return refOrException;
+    }
+
+    public @Nullable T resolveOrNull() {
+        RefOrException<T> refOrException = getRefOrException();
+        return null == refOrException.ref ? null : refOrException.ref;
+    }
+
+    public boolean isResolved() {
+        return null == getResolveException();
+    }
+
+    public @Nullable Throwable getResolveException() {
+        return getRefOrException().throwable;
+    }
+
+    private static class RefOrException<T> {
+
+        private final T ref;
+        private final Throwable throwable;
+
+        public RefOrException(@Nullable T ref, @Nullable Throwable throwable) {
+            assert null != ref || null != throwable;
+            this.ref = ref;
+            this.throwable = throwable;
+        }
+
+    }
+
+    private static class CallableFuture<T> implements Future<T> {
+
+        private final Callable<T> callable;
+
+        public CallableFuture(Callable<T> callable) {
+            this.callable = callable;
+        }
+
+        @Override
+        public boolean cancel(boolean mayInterruptIfRunning) {
+            return false;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return false;
+        }
+
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+
+        @Override
+        public T get() throws InterruptedException, ExecutionException {
+            try {
+                return callable.call();
+            } catch (Exception e) {
+                throw new ExecutionException(e);
+            }
+        }
+
+        @Override
+        public T get(long timeout, @Nonnull TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+            return get();
+        }
+    }
+
+}
--- unsafe-integration-tests/unsafe-graalvm-integration-tests/src/test/java/tools/unsafe/SampleClassTest.java
+++ unsafe-integration-tests/unsafe-graalvm-integration-tests/src/test/java/tools/unsafe/SampleClassTest.java
@@ -1,6 +1,7 @@
 package tools.unsafe;
 
 import org.junit.Test;
+import tools.unsafe.reflection.ReferenceFieldHandle;
 import tools.unsafe.reflection.x.StaticReferenceField;
 import tools.unsafe.reflection.x.StaticReferenceFieldV2;
 import tools.unsafe.reflection.x.StaticReferenceFieldV3;
@@ -10,6 +11,17 @@ import static org.junit.Assert.assertEquals;
 public class SampleClassTest {
 
     // Consider also jdk.internal.misc.Unsafe and jdk.internal.reflect.Unsafe
+
+    private final static ReferenceFieldHandle<Object> fieldHandle = new ReferenceFieldHandle<>(() -> SampleClass.class.getDeclaredField("foo"));
+
+    @Test
+    public void testReferenceFieldHandle() {
+        Object o = new Object();
+        fieldHandle.set(o);
+        assertEquals(o, SampleClass.getFoo());
+        assertEquals(SampleClass.getFoo(), fieldHandle.get());
+
+    }
 
 
     private final static StaticReferenceField REF = new StaticReferenceField(
