c778624212c306e7389c1a30e2b3609bc2bcae3e
bedrin <dmitry.bedrin@gmail.com>
2023-01-31 23:45:45 +0100

    nooooooooooooo!

--- unsafe-integration-tests/unsafe-graalvm-integration-tests/src/test/java/tools/unsafe/SampleClassTest.java
+++ unsafe-integration-tests/unsafe-graalvm-integration-tests/src/test/java/tools/unsafe/SampleClassTest.java
@@ -2,8 +2,6 @@ package tools.unsafe;
 
 import org.junit.Test;
 import tools.unsafe.reflection.ReferenceFieldHandle;
-import tools.unsafe.reflection.x.StaticReferenceFieldV2;
-import tools.unsafe.reflection.x.StaticReferenceFieldV3;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,85 +20,9 @@ public class SampleClassTest {
 
     }
 
-
-    /*private final static StaticReferenceField REF = new StaticReferenceField(
-            () -> SampleClass.class.getDeclaredField("foo"),
-            (unsafe) -> unsafe.staticFieldBase(SampleClass.class.getDeclaredField("foo")),
-            (unsafe) -> unsafe.staticFieldOffset(SampleClass.class.getDeclaredField("foo"))
-    );*/
-
     @Test
     public void testSystemProperties() {
         System.out.println(System.getProperties());
-
-    }
-
-    /*@Test
-    public void testStaticReferenceFieldV2_field() throws Throwable {
-
-        Object o = new Object();
-
-        REF.set(o);
-
-        assertEquals(o, SampleClass.getFoo());
-
-    }*/
-
-    /*@Test
-    public void testStaticReferenceField() throws Throwable {
-
-        UnsafeProvider.getSunMiscUnsafe().ensureClassInitialized(SampleClass.class);
-
-        StaticReferenceField ref = new StaticReferenceField(
-                () -> SampleClass.class.getDeclaredField("foo"),
-                (unsafe) -> unsafe.staticFieldBase(SampleClass.class.getDeclaredField("foo")),
-                (unsafe) -> unsafe.staticFieldOffset(SampleClass.class.getDeclaredField("foo"))
-        );
-
-        Object o = new Object();
-
-        ref.set(o);
-
-        assertEquals(o, SampleClass.getFoo());
-
-    }*/
-
-    @Test
-    public void testStaticReferenceFieldV2() throws Throwable {
-
-        UnsafeProvider.getSunMiscUnsafe().ensureClassInitialized(SampleClass.class);
-
-        //unsafe.ensureClassInitialized(SampleClass.class);
-
-        StaticReferenceFieldV2 ref = new StaticReferenceFieldV2(
-                UnsafeProvider.getSunMiscUnsafe().staticFieldBase(SampleClass.class.getDeclaredField("foo")),
-                UnsafeProvider.getSunMiscUnsafe().staticFieldOffset(SampleClass.class.getDeclaredField("foo"))
-        );
-
-        Object o = new Object();
-
-        ref.set(o);
-
-        assertEquals(o, SampleClass.getFoo());
-
-    }
-
-    @Test
-    public void testStaticReferenceFieldV3() throws Throwable {
-
-        UnsafeProvider.getSunMiscUnsafe().ensureClassInitialized(SampleClass.class);
-
-        //unsafe.ensureClassInitialized(SampleClass.class);
-
-        StaticReferenceFieldV3 ref = new StaticReferenceFieldV3(
-                SampleClass.class.getDeclaredField("foo")
-        );
-
-        Object o = new Object();
-
-        ref.set(o);
-
-        assertEquals(o, SampleClass.getFoo());
 
     }
 
