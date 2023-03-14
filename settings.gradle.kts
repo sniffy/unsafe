744d87589724a118194ab7c3277fa3eabcff56a8
bedrin <dmitry.bedrin@gmail.com>
2023-03-14 21:53:49 +0100

    Conditionally enabling integration tests on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,26 @@ pluginManagement {
 rootProject.name = "unsafe-parent"
 
 include("unsafe-impl")
-include("unsafe-compatibility-kit")
-include("unsafe-graalvm-integration-tests")
-include("unsafe-android-integration-tests")
-include("unsafe-ikvm-integration-tests")
-// TODO: add IKVM.NET
 
+include("unsafe-compatibility-kit")
+
+val ci = System.getenv().containsKey("CI")
+val graalVmTest = startParameter.projectProperties.containsKey("graalVmTest")
+val androidTest = startParameter.projectProperties.containsKey("androidTest")
+val dotNetTest = startParameter.projectProperties.containsKey("dotNetTest")
+
+if (ci) {
+    if (graalVmTest) {
+        include("unsafe-graalvm-integration-tests")
+    }
+    if (androidTest) {
+        include("unsafe-android-integration-tests")
+    }
+    if (dotNetTest) {
+        include("unsafe-ikvm-integration-tests")
+    }
+} else {
+    include("unsafe-graalvm-integration-tests")
+    include("unsafe-android-integration-tests")
+    include("unsafe-ikvm-integration-tests")
+}
